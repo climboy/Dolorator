efd56fff186a9c3b4b3bf69b8955e4ad7f12d037
thomaslebg70 <thomas.clavier70@yahoo.fr>
2017-02-01 11:06:22 +0100

    Dolorator
    Modification

--- list.php
+++ list.php
@@ -2,14 +2,16 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <title>Liste des Dossiers</title>
+    <title>Dolorator</title>
   </head>
   <body>
-    <h1>Liste des Dossiers</h1>
+    <h1>Dolorator</h1>
+
     <div class="container">
       <?php
-      $chemin="c:/xampp/htdocs/";/* variable donnant le chemin du dossier  de base ou l'on se trouve*/
+      $chemin="c:/wamp64/www/";/* variable donnant le chemin du dossier  de base ou l'on se trouve*/
       if(isset($_REQUEST["variable"])){/* condition permettant de vérifier si la variable "variable" existe.*/
           $path=$_REQUEST["variable"];
       }
@@ -17,11 +19,10 @@
           $path="";
       }
       $chemin=$chemin.$path;
-      echo ($path);
+
       if (is_file($chemin)) {
     OpenFile($chemin);
       }
-<<<<<<< HEAD
       else {
         $return=scandir($chemin);/* variable qui permet de scanner le dossier*/
         function decoupe($str){
@@ -35,9 +36,10 @@
             $retour = substr($str, 0, $tmp);
             return $retour;
         }
-
-
-        /* lister les dossiers*/
+        ?>
+        <img class="explo"src="./css/explorateur.png">
+        <?php
+       /* lister les dossiers*/
         foreach($return as $list){ /* boucle foreach cherche 1 par 1 les éléments de la variable return pour pouvoir les utiliser sous le nom de list*/
   if ($list == '.') {
     echo "";
@@ -45,55 +47,28 @@
             else if ($list == '..') {
                 $tmp = decoupe($path);
 
-
-               echo "<a href='?variable=".$tmp."'><img src='./css/return.png'>$list</a>";
             }
             else if (is_dir($chemin."/".$list)) {
-
-                echo "<a href=?variable=".$path."/".$list."><img src='./css/dossier.png'>$list</a>";/* affiche la liste des éléments*/
+               echo "<div class='col-md-4'><a class='soul' href=?variable=".$path."/".$list."> <img src='./css/dossier.png'>$list</a> </div>";/* affiche la liste des éléments*/
             }
             else{
-                echo "<a href=\"?variable=".$path."/".$list."\"><img src='./css/fichier.png'></a><br>";
-                echo "$list <a href=?variable='$list'></a>";/* affiche la liste des éléments*/
+                echo "<div class='col-md-4'><a class='soul' href=\"?variable=".$path."/".$list."\"> <img src='./css/fichier.png'> </a>$list <a class='soul' href=?variable='$list'></a></div>";/* affiche la liste des éléments*/
             }
         }
-=======
-       
-      
-      /* lister les dossiers*/
-      foreach($return as $list){ /* boucle foreach cherche 1 par 1 les éléments de la variable return pour pouvoir les utiliser sous le nom de list*/
-if ($list == '.') {
-  echo "";
-}
-          else if ($list == '..') {
-              $tmp = decoupe($path);
-            
-
-             echo "<a href='?variable=".$tmp."'><img src='./css/return.png'>$list</a>";
-          }
-          else if (is_dir($chemin."/".$list)) {
-
-              echo "<a href=?variable=".$path."/".$list."><img src='./css/dossier.png'>$list</a>";/* affiche la liste des éléments*/
-          }
-          else{
-              echo "<img src='./css/fichier.png'><br>";
-              echo "$list";/* affiche la liste des éléments*/
-          }
->>>>>>> origin/master
       }
+     function OpenFile($list=''){
 
-      function OpenFile($list=''){
       $descFic = fopen ($list, "r");
-      while ($ligne = fread ($descFic, filesize($list)))
+      while ($ligne = fread($descFic, filesize($list)))
 {
   print $ligne."";
 }
       fclose ($descFic);
     }
-
-      ?>
+     ?>
       </div>
 
-    </div>
+   </div>
+    <a id="return" class="col-md-6 col-md-offset-3" href="list.php"><img src="./css/return.png" alt="">Return</a>
   </body>
 </html>
